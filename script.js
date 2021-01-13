5e0f85cec3c02a8f0a03ac06e30bae12d4bbdb35
Natalya Myunster <natalyamyunster@yahoo.com>
2021-01-13 18:22:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// task1
+// var programm = prompt("Write a program which reads one string:", "JS");
+// if (programm == "JS"){
+//    alert(`Hello, ${programm}`);
+// }else {
+//     alert('You have entered some string');
+// }
+
+// task2
+// let num = -1;
+// if (num > 0){
+//     alert("+");
+// }
+// else if (num < 0){
+//     alert("-");
+// }
+// else if (num == 0){
+//     alert(0);
+// }
+// else {
+//     alert("error");
+// }
+
+// task2
+let num = 87;
+let lastNum = num.toString()[1];
+
+
